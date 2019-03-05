bc6ceed5430c331dd203e637a28de82e6a569318
Gal Agai <gal@nbv.co.il>
2019-03-05 12:06:14 +0200

    First

---
+++ src/actions/types.js
@@ -0,0 +1,7 @@
+export const SIGN_IN = "SIGN_IN_TO_G";
+export const SIGN_OUT = "SIGN_OUT_TO_G";
+export const CREATE_STREAM = 'CREATE_STREAM';
+export const FETCH_STREAMS = 'FETCH_STREAMS';
+export const FETCH_STREAM = 'FETCH_STREAM';
+export const DELETE_STREAM = 'DELETE_STREAM';
+export const EDIT_STREAM = 'EDIT_STREAM';
